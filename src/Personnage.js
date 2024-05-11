1f825f5eaf2c52914fba6c8dd9d60435e04b369a
Jonathan Vermot-Gauchy <jonathan.vermot-gauchy@etu.univ-cotedazur.fr>
2024-05-12 01:30:11 +0200

    ajout saut du personnage

--- src/Personnage.js
+++ src/Personnage.js
@@ -20,7 +20,11 @@ export class Personnage {
         this.playerRotationSpeed = 0.01;
         this.runAnimationSpeed = 3;
         this.walkAnimSpeed = 1;
+        this.jumpSpeed = 0.5;
+        this.gravity = -0.005;
         this.keyStatus = {
+            x: false,
+            Space: false,
             w: false,
             s: false,
             a: false,
@@ -28,6 +32,15 @@ export class Personnage {
             b: false,
             Shift: false
         };
+
+        this.moving = false;
+        this.jumping = false;
+
+        this.jumpHeight = 2;
+        this.jumpSpeed = 0.05;
+
+        this.currentHeight = 0;
+
         this.loadModel();
         this.setupActions();
     }
@@ -48,6 +61,10 @@ export class Personnage {
 
         this.scene.actionManager.registerAction(new ExecuteCodeAction(ActionManager.OnKeyDownTrigger, (event) => {
             let key = event.sourceEvent.key;
+            // console.log("down" + key + "-");
+            if (key == " ") {
+                this.keyStatus.Space = true;
+            }
             if (key !== "Shift") {
                 key = key.toLowerCase();
             }
@@ -59,33 +76,46 @@ export class Personnage {
 
         this.scene.actionManager.registerAction(new ExecuteCodeAction(ActionManager.OnKeyUpTrigger, (event) => {
             let key = event.sourceEvent.key;
+            // console.log("up" + key);
+            if (key == " ") {
+                this.keyStatus.Space = false;
+            }
             if (key !== "Shift") {
                 key = key.toLowerCase();
             }
             if (key in this.keyStatus) {
                 this.keyStatus[key] = false;
             }
-            console.log(this.keyStatus);
+            // console.log(this.keyStatus);
         }));
 
-        let moving = false;
+
+
         this.scene.onBeforeRenderObservable.add(() => {
 
-            if (this.keyStatus.w || this.keyStatus.s || this.keyStatus.a || this.keyStatus.d || this.keyStatus.b) {
+            if (this.keyStatus.w || this.keyStatus.s || this.keyStatus.a || this.keyStatus.d || this.keyStatus.b || this.keyStatus.Space) {
                 let speed;
                 let AnimationSpeed;
+
+
+                if (this.keyStatus.Space) { // Si la touche d'espace est enfoncée
+                    if (!this.jumping) { // Si le personnage n'est pas déjà en train de sauter
+                        this.jumping = true; // Mettre à jour l'état de saut
+                        this.jump();
+                    }
+                }
 
                 if (this.keyStatus.w || this.keyStatus.a || this.keyStatus.d) {
                     speed = this.keyStatus.Shift ? this.playerRunSpeed : this.playerWalkSpeed;
                     AnimationSpeed = this.keyStatus.Shift ? this.runAnimationSpeed : this.walkAnimSpeed;
                     this.walkAnimation.speedRatio = AnimationSpeed;
                     this.walkAnimation.start(true, AnimationSpeed, this.walkAnimation.from, this.walkAnimation.to, false);
-                    moving = true;
+                    this.moving = true;
                 }
                 if (this.keyStatus.s && !this.keyStatus.w) {
                     speed = -this.playerSpeedBackwards;
                     this.walkBackAnimation.start(true, 1, this.walkBackAnimation.from, this.walkBackAnimation.to, false);
-                    moving = true;
+                    this.moving = true;
                 }
                 if (this.keyStatus.b) {
                     this.sambaAnimation.start(true, 1, this.sambaAnimation.from, this.sambaAnimation.to, false);
@@ -96,19 +126,64 @@ export class Personnage {
                 if (this.keyStatus.d) {
                     this.player.rotate(Vector3.Up(), this.playerRotationSpeed);
                 }
-                if (moving) {
+                if (this.moving) {
                     this.player.moveWithCollisions(this.player.forward.scaleInPlace(speed));
                 }
             } else {
-                if (moving) {
+                if (this.moving) {
                     this.idleAnimeation.start(true, 1, this.idleAnimeation.from, this.idleAnimeation.tp, false);
                     this.sambaAnimation.stop();
                     this.walkAnimation.stop();
                     this.walkBackAnimation.stop();
-                    moving = false;
+                    this.moving = false;
+
                 }
             }
         });
+    }
+
+
+    jump() {
+
+
+        const jumpInterval = setInterval(() => {
+            this.currentHeight += this.jumpSpeed; // Augmenter la hauteur actuelle du saut
+            this.player.position.y = this.currentHeight; // Mettre à jour la position en fonction de la hauteur du saut
+
+            if (this.currentHeight >= this.jumpHeight) { // Si le personnage a atteint la hauteur maximale du saut
+                clearInterval(jumpInterval); // Arrêter l'interval de saut
+                setTimeout(() => {
+                    this.land();
+                }, 100);
+                // Appeler la fonction pour gérer l'atterrissage
+            }
+            // console.log(this.currentHeight);
+        }, 10); // Rafraîchir toutes les 10 ms
+    }
+
+    land() {
+        const groundHeight = 0; // Hauteur du sol
+
+        const jumpInterval = setInterval(() => {
+            this.currentHeight -= this.jumpSpeed; // Augmenter la hauteur actuelle du saut
+            this.player.position.y = this.currentHeight; // Mettre à jour la position en fonction de la hauteur du saut
+
+            if (this.currentHeight <= groundHeight) { // Si le personnage a atteint la hauteur maximale du saut
+                clearInterval(jumpInterval); // Arrêter l'interval de saut
+                this.jumping = false;
+            }
+            // console.log(this.currentHeight);
+        }, 10); // Rafraîchir toutes les 10 ms
+
+        /*
+        
+
+        // Réinitialiser la position du personnage au niveau du sol
+        this.player.position.y = groundHeight;
+
+        // Mettre à jour l'état de saut
+        this.jumping = false;
+        */
     }
 }
 
